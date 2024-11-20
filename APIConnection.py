776a8c094f5b4e501e537f064408a8496c765772
ProgrammerMichi <sergejking05@gmail.com>
2024-11-20 23:08:35 +0100

    updated apiconnection

--- APIConnection.py
+++ APIConnection.py
@@ -8,4 +8,19 @@ class TMDbAPIClient:
         self.tmdb.api_key = api_key
         self.movie_api = Movie()
 
-    def search_movie
+    def search_movie(self, query):
+        #Looks for a movie depending on title
+        return self.movie_api.search(query)
+    
+    def get_movie_details(self, movie_id):
+        #Gets the details to a film
+        return self.movie_api.details(movie_id)
+    
+    def get_recommendations(self, movie_id):
+        #Generates a recommendation based on a movie
+        return self.movie_api.recommendations(movie_id)
+    
+    def get_popular_movie(self):
+        #List of most popular films
+        return self.movie_api.popular()
+    
