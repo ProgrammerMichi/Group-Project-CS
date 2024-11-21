bb121b225b3713fba8e9accb4738378595cdaf67
ProgrammerMichi <sergejking05@gmail.com>
2024-11-21 23:59:23 +0100

    soo

--- app.py
+++ app.py
@@ -9,19 +9,39 @@ from APIConnection import TMDbAPIClient
 
 selected = st.selectbox("Select Category", options= ["Genre", "Rating", "Actor","Length", "Keywords", "Recommendation", "Popular", "Title"])
 st.write(selected)
+if selected == "Genre":
+    st.write(f"Genre")
 
+if selected == "Rating":
+    st.write(f"Rating")
 
-user_input = st.text_input("With my Infinite knowledge I shall find a Movie that contains in its name the word you enter", value = None)
-if user_input:
-    # Initialize the TMDB API client with the API key
-    testrun = TMDbAPIClient("eb7ed2a4be7573ea9c99867e37d0a4ab")
-    search_query = str(user_input)
+if selected == "Actors":
+    st.write(f"Actors")
+
+if selected == "Length":
+    st.write(f"Length")
+
+if selected == "Keywords":
+    st.write(f"Keywords")
+
+if selected == "Recommendation":
+    st.write(f"Recommendation")
+
+if selected == "Popular":
+    st.write(f"Popular")
+
+if selected == "Title":
+    user_input = st.text_input("With my Infinite knowledge I shall find a Movie that contains in its name the word you enter", value = None)
+    if user_input:
+        # Initialize the TMDB API client with the API key
+        testrun = TMDbAPIClient("eb7ed2a4be7573ea9c99867e37d0a4ab")
+        search_query = str(user_input)
     
-    # Get the movies based on the search query
-    movies = testrun.search_movie_title(search_query)
+        # Get the movies based on the search query
+        movies = testrun.search_movie_title(search_query)
     
-    # Display the movie titles
-    for movie in movies:
-        st.write(f"{movie['title']}")
+        # Display the movie titles
+        for movie in movies:
+            st.write(f"{movie['title']}")
 
 
