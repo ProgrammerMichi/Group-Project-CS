9691873860f15d483cdc547bc4e2cd8d97ce7a02
ProgrammerMichi <sergejking05@gmail.com>
2024-11-21 23:57:10 +0100

    retest

--- app.py
+++ app.py
@@ -7,21 +7,20 @@ from tmdbv3api import TMDb, Movie, Genre
 #import os
 from APIConnection import TMDbAPIClient
 
-st.selectbox: ("Select Category", 
-               options = ["Genre", "Rating", "Actors","Length", "Keywords", "Recommendation", "Popular", "Title"])
+st.selectbox("Select Category", options= ["Genre", "Rating", "Actor","Length", "Keywords", "Recommendation", "Popular", "Title"])
 
-if selected == "Title":
-    user_input = st.text_input(value = None)
-    if user_input:
-        # Initialize the TMDB API client with the API key
-        testrun = TMDbAPIClient("eb7ed2a4be7573ea9c99867e37d0a4ab")
-        search_query = str(user_input)
+
+user_input = st.text_input("With my Infinite knowledge I shall find a Movie that contains in its name the word you enter", value = None)
+if user_input:
+    # Initialize the TMDB API client with the API key
+    testrun = TMDbAPIClient("eb7ed2a4be7573ea9c99867e37d0a4ab")
+    search_query = str(user_input)
     
-        # Get the movies based on the search query
-        movies = testrun.search_movie_title(search_query)
+    # Get the movies based on the search query
+    movies = testrun.search_movie_title(search_query)
     
-        # Display the movie titles
-        for movie in movies:
-            st.write(f"{movie['title']}")
+    # Display the movie titles
+    for movie in movies:
+        st.write(f"{movie['title']}")
 
 
