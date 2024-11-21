c80ac170aa17f15e794d3327af51fd1d0d6739ef
ProgrammerMichi <sergejking05@gmail.com>
2024-11-21 22:37:49 +0100

    y

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import surprise
 import os
 from APIConnection import TMDBAPIClient
 
-#Check whether Code runs locally or on Streamlit, to decide whether .env file should be loaded
-if os.getenv("STREAMLIT_SERVER") is None:
-    from dotenv import load_dotenv
-    load_dotenv()
+#This code will check whether code runs locally or on Streamlit, to decide whether .env file should be loaded, hopefully
+# if os.getenv("STREAMLIT_SERVER") is None:
+    # from dotenv import load_dotenv
+    # load_dotenv()
 
 
 
