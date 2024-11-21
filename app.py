8baccd080c4444cd83320b5e4da8d06d05601ef2
ProgrammerMichi <sergejking05@gmail.com>
2024-11-21 22:51:22 +0100

    changes

--- app.py
+++ app.py
@@ -13,9 +13,16 @@ from APIConnection import TMDBAPIClient
 
 user_input = st.text_input("With my Infinite knowledge I shall find a Movie that contains in its name the word you enter", value = None)
 if user_input:
-    testrun = TMDBAPIClient(eb7ed2a4be7573ea9c99867e37d0a4ab)
+    # Initialize the TMDB API client with the API key
+    testrun = TMDBAPIClient("eb7ed2a4be7573ea9c99867e37d0a4ab")
     search_query = str(user_input)
+    
+    # Get the movies based on the search query
     movies = testrun.search_movie_title(search_query)
-    return st.write(for movie in movies: print(f"Title: {movie["title"]}"))
+    
+    # Display the movie titles
+    for movie in movies:
+        st.write(f"Title: {movie['title']}")
+
 
 st.write ("this updated")
