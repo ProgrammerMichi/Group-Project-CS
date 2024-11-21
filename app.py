24badd011ae9949a17d47d23f8ec17be0b41e392
ProgrammerMichi <sergejking05@gmail.com>
2024-11-21 23:19:18 +0100

    dropdown menu

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from tmdbv3api import TMDb, Movie, Genre
 #import os
 from APIConnection import TMDbAPIClient
 
+st.selectbox(Choose Category, options= ["Title", "Genre", "Actor","Length", "Keywords", "Recommendation", "Popular"])
+
 #This code will check whether code runs locally or on Streamlit, to decide whether .env file should be loaded, hopefully
 # if os.getenv("STREAMLIT_SERVER") is None:
     # from dotenv import load_dotenv
@@ -25,4 +27,3 @@ if user_input:
         st.write(f"{movie['title']}")
 
 
-st.write ("this updated again")
