605980f979f3e013c30d21ba8486135cb86a5122
ProgrammerMichi <sergejking05@gmail.com>
2024-11-21 18:44:57 +0100

    cahnges

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import os
+
+#Check whether Code runs locally or on Streamlit, to decide whether .env file should be loaded
+if os.getenv("STREAMLIT_SERVER") is None:
+    from dotenv import load_dotenv
+    load_dotenv()
+
+
+
 
 st.write ("hooray we connected anana sikim")
