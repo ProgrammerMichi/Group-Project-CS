c32c26474c06dacf40c2b4af76fc15f1ff794d88
ProgrammerMichi <sergejking05@gmail.com>
2024-11-21 22:30:09 +0100

    updated libraries

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+from tmdbv3api import TMDb, Movie, Genre
+import pandas
+import numpy
+import surprise
 import os
+from APIConnection import TMDBAPIClient
 
 #Check whether Code runs locally or on Streamlit, to decide whether .env file should be loaded
 if os.getenv("STREAMLIT_SERVER") is None:
