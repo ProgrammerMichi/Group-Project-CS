6570ae733991fbd8d1c872923b11e329efcb2d3e
hauhauu <hauyenly@outlook.com>
2024-12-09 00:05:27 +0100

    Update 5_test_file.py

--- pages/5_test_file.py
+++ pages/5_test_file.py
@@ -12,9 +12,8 @@ if st.button("See your statistics"):
 app_path = 'https://groupemichi.streamlit.app'
 page_file_path = 'pages/Ratings.py'
 page = page_file_path.split('/')[1][0:-3]  # get "1_Ratings.py"
-st.write("Rate our Recommendations") 
 st.markdown(
-    "Rate our Recommendations" f'''<a href="{app_path}/{page}" target="_self">here</a>''',
+    "Rate our Recommendations " f'''<a href="{app_path}/{page}" target="_self">here</a>''',
     unsafe_allow_html=True)
 
 # v3
