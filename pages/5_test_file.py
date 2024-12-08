9bc13089841a07fd060edf05a4521c6627c2fa49
hauhauu <hauyenly@outlook.com>
2024-12-09 00:57:53 +0100

    Update 5_test_file.py

--- pages/5_test_file.py
+++ pages/5_test_file.py
@@ -64,11 +64,13 @@ with col1:
 
           # Display movies and poster with st.container():
 for movie in movies:
-    cols=[col8,col9,coll0,col11,col12]
-    for i in range(0,5):
-        with cols[i]:
-            st.markdown(f"**{movie['title']}**")
-            st.image(movie["poster_url"], width=150)
+    with st.container()
+        col8, col9, col19, col11, col12 = st.columns(5)
+        cols=[col8,col9,coll0,col11,col12]
+            for i in range(0,5):
+                with cols[i]:
+                    st.markdown(f"**{movie['title']}**")
+                    st.image(movie["poster_url"], width=150)
 
 
 
