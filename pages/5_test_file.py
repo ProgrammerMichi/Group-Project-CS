b1d77de4cb4bb77be51ac140202192eeec461d6b
hauhauu <hauyenly@outlook.com>
2024-12-09 00:04:23 +0100

    Update 5_test_file.py

--- pages/5_test_file.py
+++ pages/5_test_file.py
@@ -1,11 +1,6 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
 
-def example():
-    want_to_contribute = st.button("I want to contribute!")
-    if want_to_contribute:
-        switch_page("Ratings")
-
+# v1
 if st.button("Get started"):
     st.switch_page("app.py")
 if st.button("Rate our Recommendations"):
@@ -13,15 +8,16 @@ if st.button("Rate our Recommendations"):
 if st.button("See your statistics"):
     st.switch_page("pages/2_Statistics.py")
 
+# v2
 app_path = 'https://groupemichi.streamlit.app'
 page_file_path = 'pages/Ratings.py'
 page = page_file_path.split('/')[1][0:-3]  # get "1_Ratings.py"
 st.write("Rate our Recommendations") 
 st.markdown(
     f'''<a href="{app_path}/{page}" target="_self">here</a>''',
-    unsafe_allow_html=True
-)
+    unsafe_allow_html=True)
 
+# v3
 url = "https://groupemichi.streamlit.app"
 st.write("get started [here](%s)" % url)
 
