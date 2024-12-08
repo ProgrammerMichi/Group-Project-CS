74b811cfcae6e80ecbea674eb8856a138758c5ee
hauhauu <hauyenly@outlook.com>
2024-12-08 23:37:46 +0100

    Update 1_Ratings.py

--- pages/1_Ratings.py
+++ pages/1_Ratings.py
@@ -9,3 +9,10 @@ st.title("How do you rate our recommendations?")
 st.write("""
     In order to improve our services for you, please rate your satisfaction with our recommendations.
     """)
+
+
+# stars for ratings
+sentiment_mapping = ["one", "two", "three", "four", "five"]
+selected = st.feedback("stars")
+if selected is not None:
+    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
