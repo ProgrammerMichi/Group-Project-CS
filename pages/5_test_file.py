24f50acc116613906ff77d83440eed8c9f9dec21
hauhauu <hauyenly@outlook.com>
2024-12-08 23:42:24 +0100

    Update 5_test_file.py

--- pages/5_test_file.py
+++ pages/5_test_file.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+if st.button("Home"):
+    st.switch_page("app.py")
+if st.button("Rate our Recommendations"):
+    st.switch_page("pages/1_Ratings.py")
+if st.button("See your statistics"):
+    st.switch_page("pages/2_Statistics.py")
+
 app_path = 'https://groupemichi.streamlit.app'
 page_file_path = 'pages/Ratings.py'
 page = page_file_path.split('/')[1][0:-3]  # get "1_Ratings.py"
