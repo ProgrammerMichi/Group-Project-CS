35bb8ccb965c0edf257d42efa8cbc1e7a93d8735
ProgrammerMichi <sergejking05@gmail.com>
2024-12-08 21:33:37 +0100

    d

--- APIConnection.py
+++ APIConnection.py
@@ -3,8 +3,6 @@ import streamlit as st
 from tmdbv3api import TMDb, Movie, Genre, Discover, Person
 
 
-
-
 #This code, in the future, should check whether code runs locally or on Streamlit, to decide whether .env file should be loaded, hopefully
 # if os.getenv("STREAMLIT_SERVER") is None:
     # from dotenv import load_dotenv
