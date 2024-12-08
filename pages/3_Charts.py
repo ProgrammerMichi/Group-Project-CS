382fc338e6be8da4d6af974923fadc4415f89a9e
hauhauu <hauyenly@outlook.com>
2024-12-08 23:07:55 +0100

    Update 3_Charts.py

--- pages/3_Charts.py
+++ pages/3_Charts.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+# Tab Title
+st.set_page_config(page_title="Charts", page_icon="📊", layout="wide")
+
+
+# Title & Intro
+st.title("Data Visualisation")
+st.write("""
+    
+    """)
