b91cc6296bacb8ef1b7744782eb45aec0cf57b1d
hauhauu <hauyenly@outlook.com>
2024-12-09 00:21:11 +0100

    Update 5_test_file.py

--- pages/5_test_file.py
+++ pages/5_test_file.py
@@ -12,6 +12,7 @@ import numpy as np
 
 Instance = TMDbAPIClient("eb7ed2a4be7573ea9c99867e37d0a4ab")
 
+
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=eb7ed2a4be7573ea9c99867e37d0a4ab&language=en-US".format(movie_id)
     data = requests.get(url)
@@ -19,6 +20,7 @@ def fetch_poster(movie_id):
     poster_path = data['poster_path']
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
+
 
 col0, col1, col2, col3, col4, col5, col6, col7, col8 = st.columns([2,2,2,2,2,2,1,3,3])
 
@@ -28,17 +30,15 @@ with col1:
         # This gives a list of movies according to which genre has been picked
         genrelist = ["Select"]
         gl = list(Instance.get_genres(any))
-        index = movies[movies['title'] == movie].index[0]
-        recommended_movie_names = []
-        recommended_movie_posters = []
     for i in gl:
-        genrelist.append(i)
-        selgen = st.selectbox("Choose Genre", options = genrelist)
+        index = movies[movies['title'] == movie].index[0]
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-        
+        genrelist.append(i)
+        genrelist.append(fetch_poster(movie_id))
+        selgen = st.selectbox("Choose Genre", options = genrelist)
 
+
+# options to switch pages      
 # v1
 if st.button("Get started"):
     st.switch_page("app.py")
