477315737420feea66cd3a76643cc371cea0b53f
hauhauu <hauyenly@outlook.com>
2024-12-08 23:27:04 +0100

    Update 5_test_file.py

--- pages/5_test_file.py
+++ pages/5_test_file.py
@@ -9,4 +9,4 @@ page = page_file_path.split('/')[1][0:-3]  # get "1_Ratings.py"
 st.write(("Rate our Recommendations") + (st.markdown(
     f'''<a href="{app_path}/{page}" target="_self">here</a>''',
     unsafe_allow_html=True
-))
+)))
