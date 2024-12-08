a64426683947f12e8360985cec7f274e0ed9eb61
hauhauu <hauyenly@outlook.com>
2024-12-08 23:17:41 +0100

    Update 5_test_file.py

--- pages/5_test_file.py
+++ pages/5_test_file.py
@@ -1,6 +1,2 @@
-from streamlit_extras.switch_page_button import switch_page
-
-def example():
-    want_to_contribute = st.button("I want to contribute!")
-    if want_to_contribute:
-        switch_page("Charts")
+if st.button("Rate our recommendations"):
+    st.switch_page("1_Ratings.py")
