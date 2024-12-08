be533b27cc803c2906f884a82a07b38e2e1f46c9
hauhauu <hauyenly@outlook.com>
2024-12-08 23:22:37 +0100

    Update 5_test_file.py

--- pages/5_test_file.py
+++ pages/5_test_file.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
-if st.write("Rate our recommendations"):
+if st.button("Rate our recommendations"):
     st.switch_page("1_Ratings.py")
+
+app_path = 'https://groupemichi.streamlit.app'
+page_file_path = 'pages/1_Ratings.py'
+page = page_file_path.split('/')[1][0:-3]  # get "page1"
+st.markdown(
+    f'''<a href="{app_path}/{page}" target="_self">goto page 1</a>''',
+    unsafe_allow_html=True
+)
