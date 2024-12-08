72fce8450409c13568c213512cf0b20f16f5b1da
hauhauu <hauyenly@outlook.com>
2024-12-08 23:10:39 +0100

    Update 4_How_this_works.py

--- pages/4_How_this_works.py
+++ pages/4_How_this_works.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+# Tab Title
+st.set_page_config(page_title="How this works", page_icon="❔", layout="wide")
+
+
+# Title & Intro
+st.title("How this works")
+st.write("""
+    
+    """)
