3d61f04ec014603b108122525a83932ba9cfda9e
ProgrammerMichi <sergejking05@gmail.com>
2024-12-08 20:39:16 +0100

    name changes

--- APIConnection.py
+++ APIConnection.py
@@ -17,27 +17,26 @@ class TMDbAPIClient:
 
         #The API_Key should be either implemented through an environment variable or streamlit/github secrets
         self.tmdb.api_key = api_key
-        self.movie_api = Movie()
-        self.genre_api = Genre()
-        self.person_api = Person()
-        self.discover_api = Discover()
+        self.movie = Movie()
+        self.genre = Genre()
+        self.person = Person()
+        self.discover = Discover()
 
 
     def search_movie_title(self, query):
         #Looks for a movie title based on query
-        return self.movie_api.search(query)
+        return self.movie.search(query)
     
     
     def get_genres(self, movie_genre):
         #Create a list with only genre names
-        genre_api = Genre()
-        genres = genre_api.movie_list()
+        genres = self.genre.movie_list()
         genre_names = [genre["name"] for genre in genres]
         return genre_names
     
     def get_genre_id(self, genre_name):
         #Gets Genre id belonging to a certain genre
-        genres = self.genre_api.movie_list()
+        genres = self.genre.movie_list()
         for genre in genres:
             if genre["name"].lower() == genre_name.lower():
                 return genre["id"]
@@ -49,7 +48,7 @@ class TMDbAPIClient:
         genre_id = self.get_genre_id(genre_name)
         
 
-        movies = self.discover_api.discover_movies({
+        movies = self.discover.discover_movies({
             "with_genres": genre_id,
             "page": page,
             "with_original_language": "en",
@@ -63,19 +62,19 @@ class TMDbAPIClient:
     
     def search_actors(self, movie_id):
         #Gets actors of a movie based on movie id
-        movie_details = self.movie_api.details(movie_id)
+        movie_details = self.movie.details(movie_id)
         return movie_details.get("cast",[])
     
     def search_movie_by_actors(self, actorname, page = 1):
         #Gets Actor ID
-        actor = self.person_api.search(actorname)
+        actor = self.person.search(actorname)
         if actor:
             actor_id = actor[0]["id"]
         if not actor:
             return []
 
         #Gets movie based on Actor ID
-        actormovies = self.discover_api.discover_movies({
+        actormovies = self.discover.discover_movies({
             "with_cast": actor_id,
             "sort_by": "vote_average_desc.",
             "page": page,
@@ -85,7 +84,7 @@ class TMDbAPIClient:
 
     def search_movie_length(self, min_length, max_length):
         #Looks for a movie depending on length
-        moviesbeforefilter = self.movie_api.popular()
+        moviesbeforefilter = self.movie.popular()
         filtered_movies = [movie for movie in moviesbeforefilter
                            if min_length <= movie["runtime"] <= max_length]
         return filtered_movies
@@ -93,22 +92,22 @@ class TMDbAPIClient:
         
     def search_movie_by_keywords(self, keyword):
         #Looks for a movie depending on keywords
-        return self.movie_api.keywords(keyword)
+        return self.movie.keywords(keyword)
     
     
     def get_movie_details(self, movie_id):
         #Gets information to a film
-        return self.movie_api.details(movie_id)
+        return self.movie.details(movie_id)
     
     
     def get_recommendations(self, movie_id):
         #Generates a recommendation based on a movie
-        return self.movie_api.recommendations(movie_id)
+        return self.movie.recommendations(movie_id)
     
     
     def get_popular_movie(self):
         #List of most popular films
-        return self.movie_api.popular()
+        return self.movie.popular()
 
 
 
