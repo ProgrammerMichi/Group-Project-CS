d84d1ed57e0a0725926ade2e552a12ed718679e7
hauhauu <hauyenly@outlook.com>
2024-12-08 23:06:25 +0100

    Update 1_Ratings.py

--- pages/1_Ratings.py
+++ pages/1_Ratings.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tab Title
-st.set_page_config(page_title="Ratings", page_icon="📋")
+st.set_page_config(page_title="Ratings", page_icon="📋", layout="wide")
 
 
 # Title & Intro
