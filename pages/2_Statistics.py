29c3b9f74ec6f63a4e72e6e09fb3f804e270da7a
hauhauu <hauyenly@outlook.com>
2024-12-08 23:08:20 +0100

    Update 2_Statistics.py

--- pages/2_Statistics.py
+++ pages/2_Statistics.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tab Title
-st.set_page_config(page_title="Statistics", page_icon="📊")
+st.set_page_config(page_title="Statistics", page_icon="📊", layout="wide")
 
 
 # Title & Intro
