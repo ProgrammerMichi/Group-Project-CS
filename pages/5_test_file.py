352a98690e6de7bd7fa78df87d400587e0061bb7
hauhauu <hauyenly@outlook.com>
2024-12-08 23:27:40 +0100

    Update 5_test_file.py

--- pages/5_test_file.py
+++ pages/5_test_file.py
@@ -6,7 +6,8 @@ if st.button("Rate our recommendations"):
 app_path = 'https://groupemichi.streamlit.app'
 page_file_path = 'pages/1_Ratings.py'
 page = page_file_path.split('/')[1][0:-3]  # get "1_Ratings.py"
-st.write(("Rate our Recommendations") + (st.markdown(
+st.write("Rate our Recommendations") 
+st.markdown(
     f'''<a href="{app_path}/{page}" target="_self">here</a>''',
     unsafe_allow_html=True
-)))
+)
