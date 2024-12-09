92b57144cd46fb95838715cc659b5ee8ca16b958
ProgrammerMichi <sergejking05@gmail.com>
2024-12-09 06:09:34 +0100

    s

--- app.py
+++ app.py
@@ -66,10 +66,10 @@ with col6:
     col6_1, col6_2 = leftbox.columns(2)
 
     with col6_1:
-        selmin_rating = st.number_input("Minimum Rating", min_value=0, max_value=110)
+        selmin_rating = st.number_input("Minimum Rating", min_value=0, max_value=100)
 
     with col6_2:
-        selmax_rating = st.number_input("Maximum Rating", min_value=0, max_value=100, value = 100)
+        selmax_rating = st.number_input("Maximum Rating", min_value=0, max_value=110, value = 100)
 
 selmin_votes = leftbox.number_input("Minimum Amount of Ratings", min_value=0, value= 1000)
     
