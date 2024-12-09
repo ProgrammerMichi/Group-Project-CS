9d1c58d6f446bd2e5d2a080e92dbb1fd6690a275
ProgrammerMichi <sergejking05@gmail.com>
2024-12-09 20:03:53 +0100

    d

--- APIConnection.py
+++ APIConnection.py
@@ -62,7 +62,7 @@ class TMDbAPIClient:
     def search_actors(self, movie_id):
         #Gets actors of a movie based on movie id
         movie_credits = self.movie.credits(movie_id)
-        actors = movie_credits["cast"]
+        actors = movie_credits["crew"]
         return_actors = []
         for actor in actors[:10]:
             return_actors.append(actor["name"])
