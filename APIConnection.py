60657f2c7dab35f91f17066fc8c49152ed2d214c
hauhauu <hauyenly@outlook.com>
2024-12-09 17:05:10 +0100

    Update APIConnection.py

--- APIConnection.py
+++ APIConnection.py
@@ -12,6 +12,14 @@ class TMDbAPIClient:
         self.discover = Discover()
 
 
+    def fetch_poster(movie_id):
+        url = "https://api.themoviedb.org/3/movie/{}?api_key=eb7ed2a4be7573ea9c99867e37d0a4ab&language=en-US".format(movie_id)
+        data = requests.get(url)
+        data = data.json()
+        poster_path = data['poster_path']
+        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+        return full_path
+    
     def search_movie_title(self, query):
         #Looks for a movie title based on query
         return self.movie.search(query)
