656e53ccfff5994736de33a3af1838d7890e90d9
ProgrammerMichi <sergejking05@gmail.com>
2024-12-10 00:59:23 +0100

    d

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ if returnmovies:
         with lc5:
             st.write("**TMDB Movie Rating**")
             st.slider("**Your Personal Rating**",min_value=1, max_value=10, key = slidercount)
-            slidercount += 1
+        slidercount += 1
