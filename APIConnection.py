3d5bc54843adebe2ccda2b1bf34ad266d191ee7e
ProgrammerMichi <sergejking05@gmail.com>
2024-12-10 00:53:17 +0100

    d

--- APIConnection.py
+++ APIConnection.py
@@ -23,10 +23,6 @@ class TMDbAPIClient:
         full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
         return full_path
     
-    def search_movie_title(self, query):
-        #Looks for a movie title based on query
-        return self.movie.search(query)
-    
     
     def get_genres(self, x = None):
         #Create a list with only genre names
@@ -41,24 +37,6 @@ class TMDbAPIClient:
             if genre["name"].lower() == genre_name.lower():
                 return genre["id"]
         return None
-    
-    
-    
-    def get_movie_by_genre_id(self, genre_name, page=1,):
-        #Looks for movies according to genre id
-        genre_id = self.get_genre_id(genre_name)
-        
-
-        movies = self.discover.discover_movies({
-            "with_genres": genre_id,
-            "page": page,
-            "with_original_language": "en",
-            "sort_by": "vote_average.desc",
-            "vote_count.gte" : 1000
-    
-        })
-        return movies
-        
     
     
     def search_actors(self, movie_id):
@@ -81,16 +59,6 @@ class TMDbAPIClient:
         if not actor:
             return("no actors found")
         
-    def search_movie_by_actor_id(self,actorname, page = 1):
-        actor_id = self.search_actor_id(actorname)
-        #Gets movie based on Actor ID
-        actormovies = self.discover.discover_movies({
-            "with_cast": actor_id,
-            "sort_by": "vote_average_desc.",
-            "page": page,
-            "with_original_language": "en"
-        })
-        return actormovies
 
     def search_movie_length(self, min_length, max_length):
         #Looks for a movie depending on length
@@ -99,25 +67,12 @@ class TMDbAPIClient:
                            if min_length <= movie["runtime"] <= max_length]
         return filtered_movies
     
-        
-    def search_movie_by_keywords(self, keyword):
-        #Looks for a movie depending on keywords
-        return self.movie.keywords(keyword)
-    
+
     
     def get_movie_details(self, movie_id):
         #Gets information to a film
         return self.movie.details(movie_id)
     
-    
-    def get_similar_movie(self, movie_id):
-        #Generates a recommendation based on a movie
-        return self.movie.recommendations(movie_id)
-    
-    
-    def get_popular_movie(self):
-        #List of most popular films
-        return self.movie.popular()
     
     def get_keyword_id(self, search):
         keywords = self.search.keywords(search)
