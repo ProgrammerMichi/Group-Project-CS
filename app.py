6bf98a2edd83b07fc6131b32c25aefe12cac9af6
ProgrammerMichi <sergejking05@gmail.com>
2024-12-09 20:27:41 +0100

    d

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ Instance = TMDbAPIClient("eb7ed2a4be7573ea9c99867e37d0a4ab")
 
 st.markdown("**hello!**")
 
-col1, col2, col3, col4, col5, col6, col7 = st.columns([2,2,2,5,5,3,3])
+col1, col2, col3, col4, col5, col6, col7 = st.columns([2,2,2,2,2,3,3])
 
 with col1:
     genre_check = st.checkbox("Genre")
