79408e35f08e04548bb04a6f36fe53e2332f95d2
ProgrammerMichi <sergejking05@gmail.com>
2024-12-09 06:08:07 +0100

    s

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ with col6:
         selmin_rating = st.number_input("Minimum Rating", min_value=0, max_value=100)
 
     with col6_2:
-        selmax_rating = st.number_input("Maximum Rating", min_value=0, max_value=100)
+        selmax_rating = st.number_input("Maximum Rating", min_value=0, max_value=100, value = 100)
 
 selmin_votes = leftbox.number_input("Minimum Amount of Ratings", min_value=0, value= 1000)
     
