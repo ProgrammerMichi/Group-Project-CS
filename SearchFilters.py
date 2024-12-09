4d8dc7842d91605ec77a4dc1e824ae1a908b5dc1
ProgrammerMichi <sergejking05@gmail.com>
2024-12-09 03:42:01 +0100

    d

--- SearchFilters.py
+++ SearchFilters.py
@@ -48,17 +48,18 @@ def findmovie(self, x):
 
     moviesfound = Instance.discover.movie(
         sort_by: "vote_average.desc",
-        with_genres: Instance.get_genre_id(genre)
-        with_cast: Instance.person.search(actor)
-        with_keywords: keywords
-        vote_average.gte: min_rating
-        vote_average.gte: max_rating
-        vote_count.gte: min_votes
-        with_runtime.gte: min_length
-        with_runtime.lte: max_length
-        primary_release_date.gte: rel_after
-        primary_release_date.lte: rel_before
+        with_genres: str(Instance.get_genre_id(genre))
+        with_cast: str(Instance.person.search(actor))
+        with_keywords: str(keywords)
+        vote_average.gte: str(min_rating)
+        vote_average.gte: str(max_rating)
+        vote_count.gte: str(min_votes)
+        with_runtime.gte: str(min_length)
+        with_runtime.lte: str(max_length)
+        primary_release_date.gte: str(rel_after)
+        primary_release_date.lte: str(rel_before)
     )
+    
     returnmovies = []
     if moviesfound:
         for movie in moviesfound:
