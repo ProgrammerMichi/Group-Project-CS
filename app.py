817a98f0a02543bfbd584c97e1209a79592056f5
hauhauu <hauyenly@outlook.com>
2024-12-10 03:39:34 +0100

    d

--- app.py
+++ app.py
@@ -89,11 +89,17 @@ with col7:
 
     midbox2 = col7.container(border=True, height=50)
 
-    m1, m2 = midbox2.columns(2)
+    m1 = midbox2.column(1)
     with m1:
         st.write("Content Rating")
-    with m2:
-        length_check = st.checkbox("Exclude PG-18 Movies")
+    
+    col7_1, col7_2 = midbox.columns(2)
+
+    with col7_1:
+        st.checkbox("Exclude PG-18 Movies")
+
+    with col7_2:
+        st.checkbox("Exclude PG-16 Movies")
 
  #ChatGPT helped with basic idea of this function(how to manage input that can be turned on/off)   
 def findmovie():
