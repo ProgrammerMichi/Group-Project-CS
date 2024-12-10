3b1ad71429a18bc551621c2a34997a383b88baa9
ProgrammerMichi <sergejking05@gmail.com>
2024-12-10 21:18:12 +0100

    d

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ with col5:
 selmin_votes = leftbox.number_input("Minimum Amount of Ratings", min_value=0, value= 1000)
 with col6:
 
-    underbox = col7.container(border=True, height=58)
+    underbox = col6.container(border=True, height=58)
     
 
 
