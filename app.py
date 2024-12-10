fe460137114b4ca3d0c999548faee44f62c1b7e4
hauhauu <hauyenly@outlook.com>
2024-12-10 04:01:47 +0100

    d

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ with col7:
         selmax_length = st.number_input("Maximum Length (in min)", min_value=0)
 
 
-    underbox = col7.container(border=True, height=120)
+    underbox = col7.container(border=True, height=130)
     m3, m4 = underbox.columns(2)
     with m3:
         st.write("Content")
