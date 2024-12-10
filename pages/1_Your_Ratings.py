79e0beb3276b25a24d5e168694680e6f6ace0d5f
ProgrammerMichi <sergejking05@gmail.com>
2024-12-10 20:29:26 +0100

    d

--- pages/1_Your_Ratings.py
+++ pages/1_Your_Ratings.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Ratings", page_icon="📋", layout="wide")
 
 
 # Title & Intro
-st.title("How do you rate our brecommendations?")
+st.title("How do you rate our recommendations?")
 st.write("""
     In order to improve our services for you, please rate your satisfaction with our recommendations.
     """)
