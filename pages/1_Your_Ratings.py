e707092fda6d664c29362333dc5c194d808f6e70
ProgrammerMichi <sergejking05@gmail.com>
2024-12-10 20:28:54 +0100

    d

--- pages/1_Your_Ratings.py
+++ pages/1_Your_Ratings.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tab Title
-st.set_page_config(page_title="Ratings", page_icon="📋", layout="wide")
+st.set_page_config(page_title="Ratiings", page_icon="📋", layout="wide")
 
 
 # Title & Intro
