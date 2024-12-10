75fcad23139f52b4ff0cb9acba13ec8d62d37b37
hauhauu <hauyenly@outlook.com>
2024-12-10 02:06:07 +0100

    d

--- app.py
+++ app.py
@@ -147,7 +147,6 @@ if returnmovies:
         with lc2:
             st.write(f"**{details.title}**")
             with st.popover("View Movie Description"):
-                description = Instance.fetch_movie_description(movie_id)
                 st.write(description)
             
         with lc3:
