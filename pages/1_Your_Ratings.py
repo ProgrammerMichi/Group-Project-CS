929a95a86987f3867faff7a6d2a77961b23d2efb
ProgrammerMichi <sergejking05@gmail.com>
2024-12-10 20:29:07 +0100

    d

--- pages/1_Your_Ratings.py
+++ pages/1_Your_Ratings.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 # Tab Title
-st.set_page_config(page_title="Ratiings", page_icon="📋", layout="wide")
+st.set_page_config(page_title="Ratings", page_icon="📋", layout="wide")
 
 
 # Title & Intro
-st.title("How do you rate our recommendations?")
+st.title("How do you rate our brecommendations?")
 st.write("""
     In order to improve our services for you, please rate your satisfaction with our recommendations.
     """)
