ef459c2d509c372763bfd1d60596d9100c0efc45
Arifiag <agon.arifi@student.unisg.ch>
2024-12-10 13:30:30 +0100

    s

--- pages/6_machinelearning.py
+++ pages/6_machinelearning.py
@@ -54,6 +54,46 @@ with col4:
 with col5:
     selorder = st.selectbox("Order of Movies by Ratings", ["Descending", "Ascending"])
 
+with col6:
+    leftbox = col6.container()
+    l1, l2 = leftbox.columns(2)
+    with l1:
+        st.write("Ratings")
+    with l2:
+        rating_check = st.checkbox("Apply Ratings")
+
+    col6_1, col6_2 = leftbox.columns(2)
+    with col6_1:
+        selmin_rating = st.number_input("Minimum Rating", min_value=0.0, max_value=10.0, step=0.1, format="%0.1f")
+    with col6_2:
+        selmax_rating = st.number_input("Maximum Rating", min_value=0.0, max_value=10.0, value=10.0, step=0.1, format="%0.1f")
+
+    selmin_votes = leftbox.number_input("Minimum Amount of Ratings", min_value=0, value=1000)
+
+with col7:
+    midbox = col7.container()
+    m1, m2 = midbox.columns(2)
+    with m1:
+        st.write("Length")
+    with m2:
+        length_check = st.checkbox("Apply Length")
+
+    col7_1, col7_2 = midbox.columns(2)
+    with col7_1:
+        selmin_length = st.number_input("Minimum Length (in min)", min_value=0)
+    with col7_2:
+        selmax_length = st.number_input("Maximum Length (in min)", min_value=0)
+
+    underbox = col7.container()
+    m3, m4 = underbox.columns(2)
+    with m3:
+        st.write("Movie Restrictions")
+    with m4:
+        st.checkbox("Apply Restriction:")
+    col7_3 = underbox.columns(1)
+    with col7_3[0]:
+        st.checkbox("Exclude 18+ Movies")
+
 # Function to fetch movies based on filters
 def findmovie():
     search_parameters = {}
@@ -70,6 +110,15 @@ def findmovie():
         search_parameters["sort_by"] = "vote_average.desc"
     else:
         search_parameters["sort_by"] = "vote_average.asc"
+
+    if rating_check:
+        search_parameters["vote_average.gte"] = str(selmin_rating)
+        search_parameters["vote_average.lte"] = str(selmax_rating)
+        search_parameters["vote_count.gte"] = str(selmin_votes)
+
+    if length_check:
+        search_parameters["with_runtime.gte"] = str(selmin_length)
+        search_parameters["with_runtime.lte"] = str(selmax_length)
 
     moviesfound = Instance.discover.discover_movies(search_parameters)
     return moviesfound
@@ -98,7 +147,6 @@ def calculate_score(movie, user_id, user_ratings, tmdb_rating_weight=0.4, person
 
 # Function to get similar movies (based on genre, actor, etc.)
 def get_similar_movies(movie):
-    # For simplicity, let's match movies by genre
     genre_ids = movie.get("genre_ids", [])
     similar_movies = user_ratings[user_ratings["movieId"].isin(
         [m["id"] for m in returnmovies if set(genre_ids).intersection(m.get("genre_ids", []))]
