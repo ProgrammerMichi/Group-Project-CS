1a80621061cd966fda9cc8e16ade73153772ffad
ProgrammerMichi <sergejking05@gmail.com>
2024-12-11 06:55:53 +0100

    d

--- app.py
+++ app.py
@@ -77,7 +77,6 @@ with col5:
 
     selmin_votes = leftbox.number_input("Minimum Amount of Ratings", min_value=0, value= 1000)
     
-    selage = leftleftbox.selectbox("", ["FSK 0", "FSK 6", "FSK 12", "FSK 16", "FSK 18"])
     
 
 
@@ -91,6 +90,7 @@ with col6:
     with ll2:
         age_check = st.checkbox ("Apply Restriction")
 
+    selage = leftleftbox.selectbox("", ["FSK 0", "FSK 6", "FSK 12", "FSK 16", "FSK 18"])
     
     
     
