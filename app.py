888180b2ed746b4faf9c0f853cac6095a7a0bcf6
ProgrammerMichi <sergejking05@gmail.com>
2024-12-11 04:13:21 +0100

    d

--- app.py
+++ app.py
@@ -26,10 +26,8 @@ with col1:
     #Dropdown menu with all available genres as options, chosen genre is saved in a variable
 
     st.write("Genre")
+    
     genrelist = Instance.get_genres(any)
-    gl = list(Instance.get_genres(any))
-    for i in Instance.get_genres(any):
-        genrelist.append(i)
     selgen = st.selectbox("Select Genre", options = genrelist.insert(0, "None"))
     
 
