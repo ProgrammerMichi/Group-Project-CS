50d99573de991c7cfb6b5082899dd655941bf02f
hauhauu <hauyenly@outlook.com>
2024-12-11 06:29:27 +0100

    d

--- pages/2_Statistics.py
+++ pages/2_Statistics.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 
 # Load the ratings data from the CSV file
@@ -7,8 +8,6 @@ ratings_df = pd.read_csv("ratings_with_genres_sample.csv")
 
 # Preprocess the data to calculate average ratings per genre
 ratings_df['primary_genre'] = ratings_df['genres'].apply(lambda x: x.split(',')[0])
-
-st.bar_chart(ratings_df['primary_genre'].set_index('genre'))
 
 # Interactive Bar Chart using Plotly (optional)
 genre_avg_ratings = ratings_df.groupby('primary_genre')['rating'].mean().reset_index()
