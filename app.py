75f3243fc20695510b414c5efb1b80c9ada60d53
ProgrammerMichi <sergejking05@gmail.com>
2024-12-11 08:54:27 +0100

    d

--- app.py
+++ app.py
@@ -19,8 +19,10 @@ st.write("")
 #Creating columns in order to have criteria options in one row next to each other
 col1, col2, col3, col4, col5, col6, col7 = st.columns([2,2,2,2,3.2,3,3])
 
-#Checkboxes coming up enable/disable inclusion of respective criteria in the search function
+#Checkboxes coming up take input on whether to enable/disable the respective criteria in the search function
 #All inputs (checkboxes and search criteria) are stored in variables, which are later used in the search function
+
+
 with col1:
     #Dropdown menu with all available genres as options, chosen genre is saved in a variable
 
@@ -32,18 +34,18 @@ with col1:
     
 
 with col2:
-    #Textfield offers option to include an actor during search
+    #Textfield takes input on which actor to include during search
 
     actor_check = st.checkbox("Include Actor")
     selactor = st.text_input("Enter Actor")
 
-    #Option to sort with descending/options ratings
+    #Input on whether to sort with descending/ascending ratings
     st.markdown("Order of Ratings")
     selorder = st.selectbox("", ["Descending", "Ascending"])
 
 
 with col3:
-    #First textfield offers option to include a keyword during search
+    #First textfield takes input (a keyword) to filter for during search
     #second one offers option to exclude a keyword
 
     keyword_check = st.checkbox("Include Keyword")
@@ -53,13 +55,13 @@ with col3:
     exclkeywords = st.text_input("Enter Keyword", key = 2)
 
 with col4:
-    
+    #Take input for option to search for movies by title, separate from other filter criteria
     title_check = st.checkbox("Search by Title")
     seltitle = st.text_input("")
     
 
 with col5:
-    #Option to filter according to minimum/maximum rating of movie
+    #Take input for option to filter according to minimum/maximum rating of movie
     #and minimum amount of votes on respective movies
 
     leftbox = col5.container(border=True, height=275)
@@ -82,7 +84,7 @@ with col5:
     
 
 with col6:
-    #Option to filter according to age rating in Germany, returns movies at or below age rating
+    #Take input on which movie ratings in Germany to filter, will return movies at or below age rating
     leftleftbox = col6.container(border=True, height = 200)
 
     ll1, ll2 = leftleftbox.columns(2)
@@ -95,7 +97,7 @@ with col6:
        
     
 with col7:
-    #Option to filter according to minimum/maximum length of movie
+    #Take input on minimum/maximum length of movie
 
     rightbox = col7.container(border=True, height=200)
 
@@ -113,23 +115,23 @@ with col7:
     with col7_2:
         selmax_length = st.number_input("Maximum Length (in min)", min_value=0)
 
+#As searching by title and by other criteria are separate, movies of certain criteria only have to be searched for 
+#when one isn't searching by title. This ensures that a list such movies will only be returned if search for title is turned off:
 if title_check == False:
     returnmovies = Instance.findmovie(selgen, actor_check, selactor, keyword_check, selkeywords, excl_check, exclkeywords, selorder, rating_check, selmin_rating, selmax_rating, selmin_votes, selmin_length, selmax_length, length_check, age_check, selage)
     Instance.movielist(returnmovies)
 
+#This makes sure that search by title and search by criteria do not interfere with each other and lets the user know if they do:
 if selgen == "None":
     selgen = False
-
 conditions = [selgen, actor_check, rating_check, keyword_check, excl_check, rating_check, age_check, length_check]
-
 
 if any(conditions) and title_check:
     st.write("You can only search for titles without any additional criteria selected")
     st.write("You can only search by criteria without any title")
 
 
+#List of movies when searching by title:
 if not any(conditions) and title_check == True and seltitle:
-    
-    st.write("helo")
     movietitle = Instance.search_movie(seltitle)
     Instance.movielist(movietitle)
