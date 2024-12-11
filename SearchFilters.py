781096b065b577c69f21aa42347467a1ecc2f3e2
ProgrammerMichi <sergejking05@gmail.com>
2024-12-11 02:19:52 +0100

    d

--- SearchFilters.py
+++ SearchFilters.py
@@ -1,63 +1,62 @@
-from tmdbv3api import Discover, Person, Genre
-from APIConnection import TMDbAPIClient
-from app import Instance
+from APIConnection import Instance
+import streamlit as st
 
-from app import selgen
-from app import selactor
-from app import selkeywords
-from app import selmin_rating
-from app import selmax_rating
-from app import selmin_votes
-from app import selmin_length
-from app import selmax_length
-from app import selrel_after
-from app import selrel_before
-from app import genre_check
-from app import actor_check
-from app import keyword_check
-from app import rating_check
-from app import length_check
-from app import date_check
-
-
-
-
-
-def findmovie():
-    if genre_check:
-        genre = selgen
+def findmovie(selgen, actor_check, keyword_check, excl_check, selorder, rating_check, length_check, selactor = None, selkeywords = None, exclkeywords = None, selmin_rating = None, selmax_rating = None, selmin_votes = None, selmin_length = None, selmax_length = None):
+    search_parameters = {}
+    if selgen != "None":
+            search_parameters["with_genres"] = str(Instance.get_genre_id(selgen))
     
-    if actor_check:
-        actor = selactor
+    if actor_check and selactor:
+        try: 
+            selactor_id = Instance.person.search(selactor + " ")
+            search_parameters["with_cast"] = str(selactor_id[0].id)
+        except: 
+            st.write("**Actor not Found**:")
+            st.write("Please adjust actor names")
+        
+        else:
+            selactor_id = Instance.person.search(selactor + " ")
+            search_parameters["with_cast"] = str(selactor_id[0].id)
 
-    if keyword_check:
-        keywords = selkeywords
+    if keyword_check and selkeywords:
+        try:
+            search_parameters["with_keywords"] = str(Instance.get_keyword_id(selkeywords))
 
+        except:
+            st.write("**Keywords not implemented in search**:")
+            st.write("Please only use one keyword. If you have already entered only one keyword, try changing it.")
+
+        else:
+            search_parameters["with_keywords"] = str(Instance.get_keyword_id(selkeywords))
+
+
+    if excl_check and exclkeywords:
+        try:
+            search_parameters["without_keywords"] = str(Instance.get_keyword_id(exclkeywords))
+
+        except:
+            st.write("False Use of Keywords:")
+            st.write("Please only use one Keyword, if you have already entered only one Keyword, try changing it.")
+
+        else:
+            search_parameters["without_keywords"] = str(Instance.get_keyword_id(exclkeywords))
+    
+    if selorder == "Descending":
+        search_parameters["sort_by"] = "vote_average.desc"
+    else:
+        search_parameters["sort_by"] = "vote_average.asc"
+    
     if rating_check:
-        min_rating = selmin_rating
-        max_rating = selmax_rating
-        min_votes = selmin_votes
+        search_parameters["vote_average.gte"] = str(selmin_rating)
+        search_parameters["vote_average.lte"] = str(selmax_rating)
+        search_parameters["vote_count.gte"] = str(selmin_votes)
 
     if length_check:
-        min_length = selmin_length
-        max_length = selmax_length
+        search_parameters["with_runtime.gte"] = str(selmin_length)
+        search_parameters["with_runtime.lte"] = str(selmax_length)
 
-    if date_check:
-        rel_after = selrel_after
-        rel_before = selrel_before
-
-    moviesfound = Instance.discover.movie({
-        "sort_by": "vote_average.desc",
-        "with_genres": str(Instance.get_genre_id(genre)),
-        "with_cast": str(Instance.person.search(actor)),
-        "with_keywords": str(keywords),
-        "vote_average.gte": str(min_rating),
-        "vote_average.lte": str(max_rating),
-        "vote_count.gte": str(min_votes),
-        "with_runtime.gte": str(min_length),
-        "with_runtime.lte": str(max_length),
-        "primary_release_date.gte": str(rel_after),
-        "primary_release_date.lte": str(rel_before),
-        })    
     
+
+    moviesfound = Instance.discover.discover_movies(search_parameters)
+
     return moviesfound
