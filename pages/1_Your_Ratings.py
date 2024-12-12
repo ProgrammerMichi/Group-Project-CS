7690da029f9fd87e0fadb7666078ede992498852
ProgrammerMichi <sergejking05@gmail.com>
2024-12-12 23:16:49 +0100

    d

--- pages/1_Your_Ratings.py
+++ pages/1_Your_Ratings.py
@@ -9,17 +9,14 @@ st.set_page_config(page_title="Your Ratings", page_icon="📋", layout="wide")
 st.title("Your Movie Ratings")
 st.write("Here you can find all the movies you have previously rated.")
 
+#Login option
 authentication.login()
 
-# stars for ratings
-sentiment_mapping = ["one", "two", "three", "four", "five"]
-selected = st.feedback("stars")
-if selected is not None:
-    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
-
-
+#Idea was to return a list of all movies the user has rated, with title and rating
 st.write(load_ratings())
 
+
+#Make sure which account you are on
 if st.session_state.get("logged_in"):
     st.write("Logged in as:", st.session_state.get("username"))
 
