c7bbe5be457851ba53707acd48e3a10017c4d4df
hauhauu <hauyenly@outlook.com>
2024-12-12 04:13:29 +0100

    d

--- pages/2_Statistics.py
+++ pages/2_Statistics.py
@@ -15,20 +15,20 @@ df_ratings = pd.read_csv("ratings_with_genres_and_details_sample.csv")
 
 # Distribution of Movie Ratings
 fig1 = px.histogram(df_ratings, x="rating", nbins=10, title="Distribution of Movie Ratings")
-fig1.show()
+st.plotly_chart(fig1)
 
 # Average Rating by Genre
 # Split genres into multiple rows
 df_genres = df_ratings.assign(genres=df_ratings['genres'].str.split(', ')).explode('genres')
 avg_rating_by_genre = df_genres.groupby('genres')['rating'].mean().reset_index()
 fig2 = px.bar(avg_rating_by_genre, x="genres", y="rating", title="Average Rating by Genre")
-fig2.show()
+st.plotly_chart(fig2)
 
 
 # Number of Movies by Release Year
 movies_by_year = df_ratings.groupby('release_year').size().reset_index(name='count')
 fig4 = px.bar(movies_by_year, x="release_year", y="count", title="Number of Movies by Release Year")
-fig4.show()
+st.plotly_chart(fig4)
 
 
 # Pie chart with genres
@@ -36,7 +36,7 @@ df_genres = df_ratings.assign(genres=df_ratings['genres'].str.split(', ')).explo
 genre_counts = df_genres['genres'].value_counts().reset_index()
 genre_counts.columns = ['genres', 'count']
 fig5 = px.pie(genre_counts, names='genres', values='count', title='Distribution of Genres')
-fig5.show()
+st.plotly_chart(fig5)
 
 # Radar chart for similarities (example using average rating by genre)
 genre_ratings = df_genres.groupby('genres')['rating'].mean().reset_index()
@@ -55,9 +55,9 @@ fig6.update_layout(
     showlegend=False,
     title='Radar Chart for Average Ratings by Genre'
 )
-fig6.show()
+st.plotly_chart(fig6)
 
 # Total movie runtime for each genre
 genre_runtime = df_genres.groupby('genres')['length'].sum().reset_index()
 fig7 = px.bar(genre_runtime, x='genres', y='length', title='Total Movie Runtime for Each Genre')
-fig7.show()
+st.plotly_chart(fig7)
