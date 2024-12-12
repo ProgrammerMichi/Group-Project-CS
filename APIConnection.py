750200cf04678c0d34982325a1d381221cc47c19
ProgrammerMichi <sergejking05@gmail.com>
2024-12-12 06:12:39 +0100

    d

--- APIConnection.py
+++ APIConnection.py
@@ -269,4 +269,4 @@ def movielist(returnmovies):
                     st.text("")
                     
                     st.slider("**Your Personal Rating**",min_value=1, max_value=10, key = movie_id)
-                    st.button("Save Rating", key = movie_id + "0")
+                    st.button("Save Rating", key = "Rating for" + movie_id)
