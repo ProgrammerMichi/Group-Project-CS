79affefa4dd94a6466de912e42ef158daa705474
hauhauu <hauyenly@outlook.com>
2024-12-12 01:53:42 +0100

    d

--- pages/1_Your_Ratings.py
+++ pages/1_Your_Ratings.py
@@ -3,14 +3,7 @@ import streamlit as st
 # Tab Title, Titles and Intro
 st.set_page_config(page_title="Ratings", page_icon="📋", layout="wide")
 st.title("Your Movie Ratings")
-st.write("...")
-
-st.header("Rate our recommendations")
-st.write("In order to improve our services for you, please rate your satisfaction with our recommendations.")
-
-
-st.header("Rate Movies you have previously watched")
-st.write("Here you can find all the movies you have already watched")
+st.write("Here you can find all the movies you have previously rated.")
 
 
 
