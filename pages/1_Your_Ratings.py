4f25fb8ed852b7a6ffbb1ce0df85cef4985a1cf4
ProgrammerMichi <sergejking05@gmail.com>
2024-12-12 21:43:33 +0100

    d

--- pages/1_Your_Ratings.py
+++ pages/1_Your_Ratings.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sqlite3
 import authentication
-from APIConnectionandRatingDB import load_ratings
+from APIConnectionandRatingDB import load_ratings, get_user_movie_ratings
 
 
 # Tab Title, Titles and Intro
@@ -17,8 +17,15 @@ selected = st.feedback("stars")
 if selected is not None:
     st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
 
-
-st.write(str(load_ratings()))
+if "logged_in" in st.session_state and st.session_state["logged_in"]:
+    movie_rating_list = get_user_movie_ratings()
+    if movie_rating_list:
+        for movie_rating in movie_rating_list:
+            st.write(movie_rating)
+    else:
+        st.write("You haven't rated any movies yet.")
+else:
+    st.warning("Please log in to see your rated movies.")
 
 if st.session_state.get("logged_in"):
     st.write("Logged in as:", st.session_state.get("username"))
