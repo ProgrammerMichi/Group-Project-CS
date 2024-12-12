6abb6e4afa9622bd6a06475383effe80c322e4a7
hauhauu <hauyenly@outlook.com>
2024-12-12 05:06:42 +0100

    d

--- pages/2_Statistics.py
+++ pages/2_Statistics.py
@@ -14,40 +14,42 @@ st.write("""Here you can find diverse charts ...""")
 df_ratings = pd.read_csv("ratings_with_genres_and_details_sample.csv")
 
 
-# Bar chart showing the average rating by genre
-df_genres = df_ratings.assign(genres=df_ratings['genres'].str.split(', ')).explode('genres')
-avg_rating_by_genre = df_genres.groupby('genres')['rating'].mean().reset_index()
-fig2 = px.bar(avg_rating_by_genre, x="genres", y="rating", title="Average Rating by Genre")
-fig2.update_layout(width=900, 
-    xaxis_title="Genres",
-    yaxis_title="Average Rating")
-st.plotly_chart(fig2)
-# Determine and display the best-rated genre
-best_rated_genre = avg_rating_by_genre.loc[avg_rating_by_genre['rating'].idxmax()]
-st.write(f"Your best-rated genre is '{best_rated_genre['genres']}' with an average rating of {best_rated_genre['rating']}.")
+col1, col2 = st.columns([1, 1])
 
+with col1:
+    # Bar chart showing the average rating by genre
+    df_genres = df_ratings.assign(genres=df_ratings['genres'].str.split(', ')).explode('genres')
+    avg_rating_by_genre = df_genres.groupby('genres')['rating'].mean().reset_index()
+    fig2 = px.bar(avg_rating_by_genre, x="genres", y="rating", title="Average Rating by Genre")
+    fig2.update_layout(width=900, 
+        xaxis_title="Genres",
+        yaxis_title="Average Rating")
+    st.plotly_chart(fig2)
+    # Determine and display the best-rated genre
+    best_rated_genre = avg_rating_by_genre.loc[avg_rating_by_genre['rating'].idxmax()]
+    st.write(f"Your best-rated genre is '{best_rated_genre['genres']}' with an average rating of {best_rated_genre['rating']}.")
 
-# Radar chart for average ratings by genre
-genre_ratings = df_genres.groupby('genres')['rating'].mean().reset_index()
-fig6 = go.Figure()
-fig6.add_trace(go.Scatterpolar(
-    r=genre_ratings['rating'],
-    theta=genre_ratings['genres'],
-    fill='toself'
-))
-fig6.update_layout(
-    polar=dict(
-        radialaxis=dict(
-            visible=True,
-            range=[0, 10],
-            tickfont=dict(color='black')
-        )),
-    showlegend=False,
-    title='Radar Chart for Average Ratings by Genre'
-)
-fig6.update_layout(width=900)
-st.plotly_chart(fig6)
-
+with col2:
+    # Radar chart for average ratings by genre
+    genre_ratings = df_genres.groupby('genres')['rating'].mean().reset_index()
+    fig6 = go.Figure()
+    fig6.add_trace(go.Scatterpolar(
+        r=genre_ratings['rating'],
+        theta=genre_ratings['genres'],
+        fill='toself'
+    ))
+    fig6.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                range=[0, 10],
+                tickfont=dict(color='black')
+            )),
+        showlegend=False,
+        title='Radar Chart for Average Ratings by Genre'
+    )
+    fig6.update_layout(width=900)
+    st.plotly_chart(fig6)
 
 
 # Bar chart showing the number of movies rated by release year
