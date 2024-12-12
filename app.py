1b4f1fefb59c3611b499cbffc11d7a9874a66103
ProgrammerMichi <sergejking05@gmail.com>
2024-12-12 13:39:06 +0100

    d

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #import numpy
 #import surprise
 #import os
-from APIConnection import get_genres, findmovie, movielist, search_movie
+from APIConnectionandRatingDB import get_genres, findmovie, movielist, search_movie
 import pandas as pd
 import numpy as np
 import authentication
