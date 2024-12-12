c12c5f00e6947a6964f16421d6579b7fe107f1e3
ProgrammerMichi <sergejking05@gmail.com>
2024-12-12 13:47:28 +0100

    d

--- pages/1_Your_Ratings.py
+++ pages/1_Your_Ratings.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import sqlite3
 
 # Tab Title, Titles and Intro
 st.set_page_config(page_title="Ratings", page_icon="📋", layout="wide")
@@ -12,3 +13,12 @@ sentiment_mapping = ["one", "two", "three", "four", "five"]
 selected = st.feedback("stars")
 if selected is not None:
     st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+
+def get_personal_ratings():
+    conn = sqlite3.connect("userratings.db")
+    cursor = conn.cursor
+    cursor.execute("SELECT * FROM userratings WHERE userId = ?", (st.session_state.get("user_id", None)))
+
+
+if st.session_state.get("Logged in", False):
+    get_personal_ratings()
