f886e5881015e38091fb3c31ef13bbbdbd86023e
ProgrammerMichi <sergejking05@gmail.com>
2024-12-12 10:39:17 +0100

    d

--- authentication.py
+++ authentication.py
@@ -7,7 +7,8 @@ cursor = conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
-    username TEXT PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,         
+    username TEXT UNIQUE,
     password TEXT
 )
 """)
