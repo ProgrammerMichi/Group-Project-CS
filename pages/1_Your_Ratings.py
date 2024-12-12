4c621654528222844297c0b4e8ae0bc6aded29c6
ProgrammerMichi <sergejking05@gmail.com>
2024-12-12 21:45:44 +0100

    d

--- pages/1_Your_Ratings.py
+++ pages/1_Your_Ratings.py
@@ -11,12 +11,6 @@ st.write("Here you can find all the movies you have previously rated.")
 
 authentication.login()
 
-# stars for ratings
-sentiment_mapping = ["one", "two", "three", "four", "five"]
-selected = st.feedback("stars")
-if selected is not None:
-    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
-
 
 st.write(str(load_ratings()))
 
