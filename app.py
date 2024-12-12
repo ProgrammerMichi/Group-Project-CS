4109bed0cfe79b9ad94b43103307e53b63e0705a
hauhauu <hauyenly@outlook.com>
2024-12-12 19:50:07 +0100

    d

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #import numpy
 #import surprise
 #import os
-from APIConnectionandRatingDB import get_genres, findmovie, movielist, search_movie, save_rating
+from APIConnectionandRatingDB import get_genres, findmovie, movielist, search_movie
 import pandas as pd
 import numpy as np
 import authentication
