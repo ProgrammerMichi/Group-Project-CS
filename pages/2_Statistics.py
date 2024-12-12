c1a08e8d33a981aab6507f7ffc7681d0b41d7b7c
hauhauu <hauyenly@outlook.com>
2024-12-12 04:51:56 +0100

    d

--- pages/2_Statistics.py
+++ pages/2_Statistics.py
@@ -14,8 +14,7 @@ st.write("""Here you can find diverse charts ...""")
 df_ratings = pd.read_csv("ratings_with_genres_and_details_sample.csv")
 
 
-# Average Rating by Genre
-# Split genres into multiple rows
+# Bar chart showing the average rating by genre
 df_genres = df_ratings.assign(genres=df_ratings['genres'].str.split(', ')).explode('genres')
 avg_rating_by_genre = df_genres.groupby('genres')['rating'].mean().reset_index()
 fig2 = px.bar(avg_rating_by_genre, x="genres", y="rating", title="Average Rating by Genre")
@@ -23,9 +22,12 @@ fig2.update_layout(width=900,
     xaxis_title="Genres",
     yaxis_title="Average Rating")
 st.plotly_chart(fig2)
+# Determine and display the best-rated genre
+best_rated_genre = avg_rating_by_genre.loc[avg_rating_by_genre['rating'].idxmax()]
+st.write(f"Your best-rated genre is '{best_rated_genre['genres']}' with an average rating of {best_rated_genre['rating']}")
 
 
-# Radar chart for average ratings by genre)
+# Radar chart for average ratings by genre
 genre_ratings = df_genres.groupby('genres')['rating'].mean().reset_index()
 fig6 = go.Figure()
 fig6.add_trace(go.Scatterpolar(
@@ -47,7 +49,7 @@ fig6.update_layout(width=900)
 st.plotly_chart(fig6)
 
 
-# Number of Movies by Release Year
+# Bar chart showing the number of movies rated by release year
 movies_by_year = df_ratings.groupby('release_year').size().reset_index(name='count')
 fig4 = px.bar(movies_by_year, x="release_year", y="count", title="Number of Movies Rated by Release Year")
 fig4.update_layout(
@@ -57,7 +59,7 @@ fig4.update_layout(
 st.plotly_chart(fig4)
 
 
-# Pie chart with genres
+# Pie chart showing the portion of genres for all rated movies
 df_genres = df_ratings.assign(genres=df_ratings['genres'].str.split(', ')).explode('genres')
 genre_counts = df_genres['genres'].value_counts().reset_index()
 genre_counts.columns = ['genres', 'count']
@@ -66,7 +68,7 @@ fig5.update_layout(width=900)
 st.plotly_chart(fig5)
 
 
-# Total movie runtime for each genre
+# Bar Chart showing the total movie runtime for each genre
 genre_runtime = df_genres.groupby('genres')['length'].sum().reset_index()
 fig7 = px.bar(genre_runtime, x='genres', y='length', title='Total Movie Runtime for Each Genre')
 fig7.update_layout(width=900,
